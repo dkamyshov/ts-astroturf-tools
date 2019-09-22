6b23f738ad683340c89a413eca836b2435f5216f
dkamyshov <danil.kamyshov@gmail.com>
2019-09-22 18:41:55 +0300

    reorganize the package

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   collectCoverage: true,
   collectCoverageFrom: [
-    '*.ts',
-    'utils/*.ts',
-    '!utils/constants.ts',
-    '!utils/getSourceFile.ts',
+    'src/**/*.ts',
+    '!src/utils/constants.ts',
+    '!src/utils/getSourceFile.ts',
   ],
 };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,15 @@
+/**
+ * Regexp used to extract CSS class names
+ * from CSS sources.
+ */
 export const tokensRegExp = /\.[\w\-]+/g;
+
+/**
+ * Regexp used to clear CSS source of
+ * template placeholders.
+ */
 export const literalsRegExp = /\$\{.*\}/g;
+
 export const packageName = 'ts-astroturf-tools';
 export const missingIdentifierCode = 1;
 export const failedToExecuteCode = 2;
