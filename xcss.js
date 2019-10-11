4bf3c95b2d24baa863bd2371c4d5ba8bf5673ccb
dkamyshov <danil.kamyshov@gmail.com>
2019-10-12 02:53:18 +0300

    revert to older syntax

--- xcss.js
+++ xcss.js
@@ -1,7 +1,7 @@
 module.exports = {
   xcss: function() {
     throw new Error(
-      `[ts-astroturf-tools/xcss] 'xcss' is not supposed to be called at runtime. Check your configuration.`
+      "[ts-astroturf-tools/xcss] 'xcss' is not supposed to be called at runtime. Check your configuration."
     );
   },
 };
